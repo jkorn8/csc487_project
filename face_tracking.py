ef27fc32de76512336fee0d98cc0e7ebd32c2d80
sgcook <sgcook@calpoly.edu>
2024-03-04 14:31:53 -0800

    Created new model to test, changed helpers, made file to test data preprocessing

--- face_tracking.py
+++ face_tracking.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import os
 import time
+from helpers import display_progressbar
 
 TIMESTEP_FOR_IMAGE_DOWNLOAD = 0
 MODEL_INPUT_IMAGE_DIMENSIONS = (250, 250)
@@ -49,6 +50,23 @@ def face_detection_helper(collect_data, output_directory_path='test_images', max
         face_detector.setInput(blob)
         detections = face_detector.forward()
 
+        if collect_data:
+            c = -1
+            k = -1
+            for j in range(0, detections.shape[2]):
+                if detections[0, 0, j, 2] > c:
+                    c = detections[0, 0, j, 2]
+                    k = j
+            if c > .8:
+                box = detections[0, 0, k, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+                t2 = time.time()
+                if (t2 - t1) > TIMESTEP_FOR_IMAGE_DOWNLOAD:
+                    collect_data_helper(image=image, coords=(max(startX, 0), max(startY, 0), endX, endY),
+                                        output_directory_path=output_directory_path, im_num=im_num)
+                    im_num += 1
+                    t1 = time.time()
+
         for i in range(0, detections.shape[2]):
             confidence = detections[0, 0, i, 2]
 
@@ -59,29 +77,58 @@ def face_detection_helper(collect_data, output_directory_path='test_images', max
 
         cv2.imshow("Output", image)
 
-        if collect_data:
-            c = -1
-            k = -1
-            for j in range(0, detections.shape[2]):
-                if detections[0, 0, j, 2] > c:
-                    c = detections[0, 0, j, 2]
-                    k = j
-            if c > .5:
-                box = detections[0, 0, k, 3:7] * np.array([w, h, w, h])
-                (startX, startY, endX, endY) = box.astype("int")
-                t2 = time.time()
-                if (t2 - t1) > TIMESTEP_FOR_IMAGE_DOWNLOAD:
-                    collect_data_helper(image=image, coords=(max(startX, 0), max(startY, 0), endX, endY),
-                                        output_directory_path=output_directory_path, im_num=im_num)
-                    im_num += 1
-                    t1 = time.time()
-
         if (cv2.waitKey(10) & 0xFF == ord('q')) or (collect_data and max_num_images <= im_num):
             cap.release()
             cv2.destroyAllWindows()
             for i in range(2):
                 cv2.waitKey(1)
             break
+
+
+def process_unknown_faces(input_path, output_path, face_detector):
+    t_orig = time.time()
+    total_photos = len(os.listdir(input_path))
+    total_processed = 0
+    t = time.time()
+    exceptions = []
+    print("Processing {} photos...".format(total_photos))
+    for face in os.listdir(input_path):
+        if str.endswith(face, '.jpg'):
+            image = cv2.imread(str(os.path.join(input_path, face)))
+
+            (h, w) = image.shape[:2]
+
+            blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+
+            face_detector.setInput(blob)
+            detections = face_detector.forward()
+
+            confidence = -1
+            i = 0
+            for j in range(0, detections.shape[2]):
+                if detections[0, 0, j, 2] > confidence:
+                    confidence = detections[0, 0, j, 2]
+                    i = j
+
+            if confidence > .9:
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+                try:
+                    image = cv2.resize(image[max(startY, 0):endY, max(startX, 0):endX],
+                                       MODEL_INPUT_IMAGE_DIMENSIONS,
+                                       interpolation=cv2.INTER_LINEAR)
+                    if not cv2.imwrite(str(os.path.join(output_path, face)), image):
+                        print("FAILED !!!!!")
+                except Exception as e:
+                    exceptions.append(e)
+            total_processed += 1
+            t = display_progressbar(t1=t, t_orig=t_orig, total_photos=total_photos, total_processed=total_processed)
+
+    print("\r\nDone!")
+    if len(exceptions) > 0:
+        print("Some exceptions occurred:")
+        for e in exceptions:
+            print(e)
 
 
 def face_detection():
--- helpers.py
+++ helpers.py
@@ -1,10 +1,9 @@
-import os
 import numpy as np
-import imageio.v3 as iio
 import sys
 import time
-import tensorflow as tf
 import random
+from keras.utils import load_img, img_to_array
+from keras.applications import vgg19
 
 '''
 Helper function for displaying the process current progress of the process
@@ -67,62 +66,20 @@ Input:
 Output: 
     Array of size "size", "percent_distribution" percent randomly filled with 1's, rest filled with 0's
 '''
-def get_distribution_array(size, percent_distribution):
-    if percent_distribution < 0 or percent_distribution > 1:
-        raise ValueError('percent_distribution must be between 0 and 1')
-    number_of_1s = int((size * percent_distribution) // 1)
-    arr = [1] * number_of_1s + [0] * (size-number_of_1s)
+def get_distribution_array(size, percent_training, percent_validation, percent_testing):
+    # Training is 0
+    # Validation is 1
+    # Testing is 2
+    if percent_training <= 0 or percent_validation <= 0 or percent_testing <= 0 or percent_training + percent_validation + percent_testing > 100:
+        raise ValueError('percent_training, percent_validation, and percent_testing must add to 100')
+    number_of_0s = int((size * percent_training) // 1)
+    number_of_1s = int((size * percent_validation) // 1)
+    number_of_2s = int((size * percent_testing) // 1)
+    arr = [0] * (number_of_0s) + [1] * (number_of_1s) + [2] * number_of_2s
+    while len(arr) < size:
+        arr.append(0)
     random.shuffle(arr)
     return arr
-
-
-'''
-Function to augment data to add data reflection and/or rotations. Adds this augmented data to the input arrays
-Input:
-    Images (image tensor list): array of images you wish to augment
-    labels (integer list): array of labels for the images
-    mirror (bool): whether to do mirror augmentations
-    num_rotate (int): non-negative number of rotation augmentations you wish to do per image
-Output:
-    None
-'''
-MAX_ROTATION_DEGREES = 15  # Between 0 and 360
-def augment_data(images, labels, mirror=True, num_rotate=1):
-    t1 = time.time()
-    t_orig = time.time()
-    if mirror:
-        total_photos = len(images)
-        total_processed = 0
-        print("Mirroring images...")
-        for i in range(len(images)):
-            images.append(tf.image.flip_left_right(images[i]))
-            labels.append(labels[i])
-            total_processed += 1
-            t1 = display_progressbar(t1, t_orig, total_photos, total_processed)
-        sys.stdout.flush()
-        sys.stdout.write("\r")
-        print(str(total_processed) + '/' + str(total_photos) + " "
-              + get_progressbar(total_processed, total_photos)
-              + " - ETA: " + get_minutes_from_seconds(0))
-        print("Done mirroring images!")
-
-    if num_rotate > 0:
-        rotate_image = tf.keras.layers.RandomRotation(factor=MAX_ROTATION_DEGREES/360)
-        total_photos = len(images)
-        total_processed = 0
-        print("Rotating images...")
-        for i in range(len(images)):
-            for j in range(num_rotate):
-                images.append(rotate_image(images[i]))
-                labels.append(labels[i])
-            total_processed += 1
-            t1 = display_progressbar(t1, t_orig, total_photos, total_processed)
-        sys.stdout.flush()
-        sys.stdout.write("\r")
-        print(str(total_processed) + '/' + str(total_photos) + " "
-              + get_progressbar(total_processed, total_photos)
-              + " - ETA: " + get_minutes_from_seconds(0))
-        print("Done rotating images!")
 
 
 '''
@@ -133,8 +90,8 @@ Input:
 Output:
     None
 '''
-def shuffle_arrays(l1, l2):
-    if len(l1 != len(l2)):
+def shuffle_arrays_in_sync(l1, l2):
+    if len(l1) != len(l2):
         raise ValueError("Arrays must be same length")
     arr = []
     l1_final = [0]*len(l1)
@@ -149,129 +106,20 @@ def shuffle_arrays(l1, l2):
     return l1_final, l2_final
 
 
-'''
-Function that retrieves images and processes them as numpy vectors
-Input: num_faces (int): number of total identities you wish to process;
-    num_unknown (int): number of identities you wish to label as "unknown" (-1);
-    percent_training_data (float): percent of data you wish to be training data;
-    distribution (string): distribution of training data, must be either "even" or "random"
-        if "even", then use every identity, but only use "percent_training_data" percent of photos from each person for training data
-        if "random", then randomly select photos from pool of all photos for training data
-    add_mirrors (bool): whether augment data by adding mirrored images
-    num_rotations (int): non-negative number of augmented data rotations of each image to add to data
-Output:
+def preprocess(input_path, dimensions):
+    img = load_img(input_path, target_size=dimensions)
+    img = img_to_array(img)
+    img = vgg19.preprocess_input(img)
+    return img
 
-WARNING: Having too much data causes SIGKILL.
-'''
-def get_data(num_faces=10000, num_unknown=0, percent_training_data=10.0, 
-             distribution="even", add_mirrors=True, num_rotations=1):
 
-    # Check for valid input
-    if not isinstance(num_faces, int):
-        raise ValueError('num_faces must be an integer')
-    if not isinstance(num_unknown, int):
-        raise ValueError('num_unknown must be an integer')
-    if not (isinstance(percent_training_data, float) or isinstance(percent_training_data, int)):
-        raise ValueError('percent_training_data must be an float')
-    if distribution not in ["even", "random"]:
-        raise ValueError('distribution must be "even" or "random"')
-    if num_faces < 1 or num_faces > 10000:
-        raise ValueError('num_faces must be between 1 and 10000')
-    if percent_training_data > 100 or percent_training_data < 0:
-        raise ValueError('percent_training_data must be between 0 and 100')
-    if num_faces <= num_unknown:
-        raise ValueError("Number of total faces must be greater than number of unknown faces")
-    if not (isinstance(add_mirrors, bool)):
-        raise ValueError('add_mirrors must be True or False')
-    if not (isinstance(num_rotations, int)):
-        raise ValueError('num_rotations must be an integer')
-    if num_rotations < 0:
-        raise ValueError('num_rotations must be 0 or greater')
-
-    # Initialize variables
-    test_images = []
-    test_labels = []
-    training_images = []
-    training_labels = []
-    do_process_photo = []
-    t1 = time.time()
-    t_orig = time.time()
-    total_photos = 72*num_faces   # Should edit if adding/removing photos
-    total_processed = 0
-    total_people_processed = 0
-    j = 0
-    identity_mapper = dict()
-    if num_unknown == 0:
-        size_of_last_layer = num_faces
-    else:
-        size_of_last_layer = num_faces - num_unknown + 1
-    if distribution == "random":
-        do_process_photo = get_distribution_array(num_faces*72, 1 - percent_training_data/100)
-
-    if not (os.path.exists('photos') and os.path.isdir('photos')):
-        raise FileNotFoundError('Error: Missing "photos" directory')
-
-    print("Beginning processing of images:")
-
-    print("Retrieving images...")
-    sys.stdout.write('\r' + str(total_processed) + '/' + str(total_photos)
-                     + " " + get_progressbar(total_processed, total_photos))
-
-    # Note: photos directory should hold directories that include directories that include pngs
-    #   photos - person 0 - photo 0
-    #      |         |------ photo 1
-    #      |         ...
-    #      |         |------ photo k-1
-    #      |----- person 1
-    #      ...
-    #      |----- person m-1
-    identities = os.listdir('photos')
-    random.shuffle(identities)
-    for i in range(len(identities)):
-        if os.path.isdir('photos/' + identities[i]):
-            identity_mapper[i] = identities[i]
-            if distribution == "even":
-                do_process_photo = get_distribution_array(72, 1 - percent_training_data / 100)
-            for photo in os.listdir('photos/' + identities[i]):
-                if photo.endswith('.png'):
-                    if do_process_photo[j] == 1:
-                        test_images.append(iio.imread('photos/' + identities[i] + '/' + photo)[:, :, :3])  # Removing transparency layer
-                        if num_faces - num_unknown <= total_people_processed:
-                            test_labels.append(num_faces - num_unknown)
-                        else:
-                            test_labels.append(i)
-                    else:
-                        training_images.append(iio.imread('photos/' + identities[i] + '/' + photo)[:, :, :3])  # Removing transparency layer
-                        if num_faces - num_unknown <= total_people_processed:
-                            training_labels.append(num_faces - num_unknown)
-                        else:
-                            training_labels.append(i)
-                    total_processed += 1
-                    j += 1
-                t1 = display_progressbar(t1, t_orig, total_photos, total_processed)
-            if distribution == "even":
-                j = 0
-            total_people_processed += 1
-        if total_people_processed >= num_faces:
-            break
-
-    # Cleanup and get output
-    sys.stdout.flush()
-    sys.stdout.write("\r")
-    print(str(total_processed) + '/' + str(total_photos) + " "
-          + get_progressbar(total_processed, total_photos)
-          + " - ETA: " + get_minutes_from_seconds(0))
-    print("Done Retrieving images!")
-    del do_process_photo, t1, t_orig, total_photos, total_processed, total_people_processed, j
-
-    if add_mirrors or (num_rotations >= 1):
-        augment_data(test_images, test_labels, add_mirrors, num_rotations)
-        # augment_data(training_images, training_labels, add_mirrors, num_rotations)
-
-    print("Converting to numpy arrays...")  # Appending to python array is faster than appending to numpy array
-    test_images, test_labels = shuffle_arrays(test_images, test_labels)
-    training_images, training_labels = shuffle_arrays(training_images, training_labels)
-    test_images, test_labels = np.array(test_images), np.array(test_labels)
-    training_images, training_labels = np.array(training_images), np.array(training_labels)
-    print("Done Processing images!")
-    return (test_images, test_labels), (training_images, training_labels), identity_mapper, size_of_last_layer
+def deprocess_image(x):
+    # Remove zero-center by mean pixel
+    x[:, :, 0] += 103.939
+    x[:, :, 1] += 116.779
+    x[:, :, 2] += 123.68
+    # 'BGR'->'RGB'
+    x = x[:, :, ::-1]
+    x = np.clip(x, 0, 255).astype('uint8')
+    x = x/255
+    return x
